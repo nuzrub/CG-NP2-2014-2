c98fc179c66e21313ddf4d3ac2c39421d8862064
YgorReboucas <ygor.reboucas@gmail.com>
2014-11-07 22:41:06 -0300

    Configurando o projeto, colocando um cenário de introdução e o personagem que usaremos para testes.

---
+++ Assets/Scripts/MovimentoHumanoide.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimentoHumanoide : MonoBehaviour {
+
+    private CharacterController controller;
+    private float movementSpeed = 2f;
+    private float rotationSpeed = 180f;
+    private float gravityAccel = -0.25f;
+    private float jumpHeight = 0.10f;
+    private float jumpInterval = 0.4f;
+
+    private float verticalVelocity;
+    private float lastJump;
+
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        verticalVelocity = 0f;
+
+        jumpInterval += -Mathf.Sqrt(-2 * gravityAccel * jumpHeight) / gravityAccel;
+        lastJump = -jumpInterval;
+	}
+	
+
+	void Update () {
+        Rotacionar();
+        Movimentar();
+	}
+
+    private void Rotacionar() {
+        float rotation = rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Rotate(Vector3.up, rotation);
+    }
+    private void Movimentar() {
+        float movement = movementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        if (controller.isGrounded) {
+            if (Input.GetButton("Jump") && verticalVelocity < 0.1f && (Time.time > (lastJump + jumpInterval))) {
+                verticalVelocity = Mathf.Sqrt(-2 * gravityAccel * jumpHeight) / 2f;
+                lastJump = Time.time;
+            } else {
+                verticalVelocity = 0;
+            }
+        } else {
+            verticalVelocity += gravityAccel * Time.deltaTime;
+        }
+
+
+        if (movement != 0) {
+            animation.CrossFade("run");
+        } else {
+            animation.CrossFade("idle");
+        }
+
+        
+        controller.Move(
+            transform.forward * movement + 
+            transform.up * verticalVelocity);
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    Transform respawnPoint;
+
+    void Start() {
+        respawnPoint = transform.GetChild(0);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        other.transform.position = respawnPoint.position;
+        other.transform.rotation = respawnPoint.rotation;
+    }
+}

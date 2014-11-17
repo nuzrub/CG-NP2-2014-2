5c8ed41e3883bd4ceda08d091741996bb6f4387a
Victor <victorcdcc@gmail.com>
2014-11-16 22:41:42 -0300

    Adicionado script para mudança da perspectiva da camera.

---
+++ Assets/Scripts/PerspectiveSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerspectiveSwitcher : MonoBehaviour
+{
+	bool ortho = true;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.C))
+		{
+			ortho = !ortho;
+			if (ortho)
+			camera.orthographic = true;
+			else
+				camera.orthographic = false;
+		}
+	}
+}

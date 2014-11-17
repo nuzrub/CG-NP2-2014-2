5371fa88aa96b050cbb0a80279640d9b94c54164
Yandre Rodrigues <yandre.rco@gmail.com>
2014-11-17 18:46:40 -0200

    script de rotação de câmera usando as teclas 'z' e 'x'

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	public int speed;
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Z)) {
+			transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.X)) {
+			transform.RotateAround(Vector3.zero, Vector3.down, speed * Time.deltaTime);
+		}
+	}
+}

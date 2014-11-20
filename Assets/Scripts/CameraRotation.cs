806d5818507e854467ea71d012ce41d4b9f66b8b
YgorReboucas <ygor.reboucas@gmail.com>
2014-11-19 23:17:53 -0300

    Camera Rotation OK! Corrigi o pivot de rotação da câmera, antes estava Vector3.zero, troquei para transform.parent.position. Isso faz com que a câmera gire em torno do jogador ao invés do (0, 0, 0). Adicionei uma funcionalidade para resetar a posição e orientação da câmera ao clicar R

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -3,14 +3,28 @@ using System.Collections;
 
 public class CameraRotation : MonoBehaviour {
 
+    public Vector3 initialPos;
+    public Quaternion initialRot;
 	public int speed;
 
+    void Start() {
+        initialPos = transform.localPosition;
+        initialRot = transform.localRotation;
+    }
+
 	void Update () {
+        Vector3 pivot = transform.parent.position;
+
 		if (Input.GetKey (KeyCode.Z)) {
-			transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+            transform.RotateAround(pivot, Vector3.up, speed * Time.deltaTime);
 		}
 		if (Input.GetKey (KeyCode.X)) {
-			transform.RotateAround(Vector3.zero, Vector3.down, speed * Time.deltaTime);
+            transform.RotateAround(pivot, Vector3.down, speed * Time.deltaTime);
 		}
+
+        if (Input.GetKey(KeyCode.R)) {
+            transform.localPosition = initialPos;
+            transform.localRotation = initialRot;
+        }
 	}
 }

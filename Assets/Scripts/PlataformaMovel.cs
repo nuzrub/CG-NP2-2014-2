f4c87fae79d1140d5feab13379afdca9edcaa83e
Yandre Rodrigues <yandre.rco@gmail.com>
2014-11-23 18:05:44 -0200

    efeito de fumaça ao respawnar

---
+++ Assets/Scripts/PlataformaMovel.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlataformaMovel : MonoBehaviour {
+
+	[SerializeField]
+	Transform plataforma;
+
+	[SerializeField]
+	Transform inicioTransform;
+
+	[SerializeField]
+	Transform finalTransform;
+
+	[SerializeField]
+	float speed;
+
+	Vector3 direcao;
+	Transform destino;
+
+	void Start()
+	{
+		setDestino (inicioTransform);
+	}
+
+	void FixedUpdate()
+	{
+		plataforma.rigidbody.MovePosition (plataforma.position + direcao * speed * Time.fixedDeltaTime);
+
+		if (Vector3.Distance (plataforma.position, destino.position) < speed * Time.fixedDeltaTime) 
+		{
+			setDestino(destino == inicioTransform ? finalTransform : inicioTransform);
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube (inicioTransform.position, plataforma.localScale);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube (finalTransform.position, plataforma.localScale);
+	}
+
+	void setDestino(Transform dest)
+	{
+		destino = dest;
+		direcao = (destino.position - plataforma.position).normalized;
+	}
+
+}
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -2,15 +2,17 @@
 using System.Collections;
 
 public class Portal : MonoBehaviour {
-
-    Transform respawnPoint;
-
-    void Start() {
-        respawnPoint = transform.GetChild(0);
-    }
-
-    void OnTriggerEnter(Collider other) {
-        other.transform.position = respawnPoint.position;
-        other.transform.rotation = respawnPoint.rotation;
-    }
+	Transform smokePoint;
+	Transform respawnPoint;
+	public GameObject smoke;
+	void Start() {
+		respawnPoint = transform.GetChild(0);
+		smokePoint = transform.GetChild (1);
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		Instantiate(smoke,smokePoint.position,smokePoint.rotation);
+		other.transform.position = respawnPoint.position;
+		other.transform.rotation = respawnPoint.rotation;
+	}
 }

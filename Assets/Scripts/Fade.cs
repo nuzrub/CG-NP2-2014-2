aa741a32d0793661e548d083fc2b5edb82331c98
Marina de Oliveira Gosson <marina.gosson@gmail.com>
2014-11-23 01:02:13 -0300

    Alteracao na classe de Fade. Controles: M, aumenta a claridade. N, diminui a claridade, deixando apenas a luz do holofote. Acrescimo da tocha e da fogueria.

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour {
+
+	        //. ....
+		    //.  O
+			//. /|\
+			//.  |
+			//. / \
+			//.
+			//=========
+
+	public Light luz;
+	public float intensidade;
+	public float velocidade = 1.0f;
+
+	public float limiteSuperior = 1.0f;
+	public float limiteInferior = 0.0f;
+
+	void Start()
+	{
+		luz.intensity = 0.0f;
+	}
+
+	// Update is called once per frame (formatacao xapada)
+	void Update () {
+		if (Input.GetKey(KeyCode.M)) {
+			if (luz.intensity < limiteSuperior) {
+				luz.intensity = luz.intensity + (velocidade * Time.deltaTime);
+				intensidade = luz.intensity;
+			}
+		} 
+
+		if (Input.GetKey(KeyCode.N)) {
+					
+			if(luz.intensity > limiteInferior)	{
+				luz.intensity = luz.intensity - (velocidade * Time.deltaTime);
+				intensidade = luz.intensity;
+			}
+
+		}
+
+	}
+	
+}
---
+++ Assets/Scripts/Fade_Ney.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade_Ney : MonoBehaviour {
+
+
+	// 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame (nice!)
+	void Update () {
+
+
+
+	}
+}

1625c44df59081f84c5bbe486ecb687cdf1f4450
YgorReboucas <ygor.reboucas@gmail.com>
2014-11-24 21:59:09 -0300

    Pequena modificação no evento do Yandre

--- Assets/Scripts/Evento_Yandre.cs
+++ Assets/Scripts/Evento_Yandre.cs
@@ -2,53 +2,57 @@
 using System.Collections;
 
 public class Evento_Yandre : MonoBehaviour {
-	
-	public Transform dragaoPrefab;
-	public Transform dragaoStart;
-	public Transform dragaoEnd;
-	public CameraRotation cameraRotation;
-	public Light luz;
-	
-	private DragaoScript dragao;
-	private MovimentoHumanoide player;
-	private float cronometro;
-	private bool comecou;
 
-	void OnTriggerEnter(Collider other)	{
-		if (!comecou) {
-			comecou = true;
-			player = other.GetComponent<MovimentoHumanoide>();
-			player.Habilitado = false;
+    public Transform dragaoPrefab;
+    public Transform dragaoStart;
+    public Transform dragaoEnd;
+    public CameraRotation cameraRotation;
+    public Light luz;
 
-			Transform dragaoObject = (Transform)Instantiate(dragaoPrefab, dragaoStart.position, dragaoStart.rotation);
-			dragao = dragaoObject.GetComponent<DragaoScript>();
-			dragao.Voar();
-			dragao.transform.RotateAround(dragao.transform.position, Vector3.up,225);
+    private DragaoScript dragao;
+    private MovimentoHumanoide player;
+    private float cronometro;
+    private bool comecou;
 
-		}
-	}
+    void OnTriggerEnter(Collider other) {
+        if (!comecou) {
+            comecou = true;
+            player = other.GetComponent<MovimentoHumanoide>();
+            player.Habilitado = false;
 
-	void Update ()	{
-		if (comecou) {
-			cronometro += Time.deltaTime;
+            Transform dragaoObject = (Transform)Instantiate(dragaoPrefab, dragaoStart.position, dragaoStart.rotation);
+            dragao = dragaoObject.GetComponent<DragaoScript>();
+            dragao.Voar();
+            dragao.transform.RotateAround(dragao.transform.position, Vector3.up, 225);
 
-			if(cronometro<6){
-				dragao.transform.position = Vector3.Lerp(dragaoStart.position, dragaoEnd.position,cronometro / 6f);
-				Camera.main.transform.position = dragao.transform.position - new Vector3(1.5f, -2f, 6f);
-				Camera.main.transform.LookAt(dragao.transform); 
-			
-			}else if(cronometro < 10)	{
-				Camera.main.transform.position = player.transform.position - new Vector3(0.5f,-0.1f,-0.3f);
-				Camera.main.transform.LookAt(player.transform);
+        }
+    }
 
-			}else {
-				cameraRotation.ResetCamera();
-				player.Habilitado = true;
-				Destroy(dragao.gameObject);
-				Destroy (this.gameObject);
-			}
+    void Update() {
+        if (comecou) {
+            cronometro += Time.deltaTime;
+
+            if (cronometro < 3.5f) {
+                dragao.transform.position = Vector3.Lerp(dragaoStart.position, dragaoEnd.position, cronometro / 6f);
+                Camera.main.transform.position = dragao.transform.position - new Vector3(1.5f, -2f, 6f);
+                Camera.main.transform.LookAt(dragao.transform);
+            } else if (cronometro < 6) {
+                dragao.transform.position = Vector3.Lerp(dragaoStart.position, dragaoEnd.position, cronometro / 6f);
+                Camera.main.transform.position = dragao.transform.position - new Vector3(1.5f, -2f, 6f);
+                Camera.main.transform.LookAt(dragao.transform);
+                dragao.UsarFogo();
+            } else if (cronometro < 10) {
+                Camera.main.transform.position = player.transform.position - new Vector3(0.5f, -0.1f, -0.3f);
+                Camera.main.transform.LookAt(player.transform);
+
+            } else {
+                cameraRotation.ResetCamera();
+                player.Habilitado = true;
+                Destroy(dragao.gameObject);
+                Destroy(this.gameObject);
+            }
 
 
-		}
-	}
+        }
+    }
 }
